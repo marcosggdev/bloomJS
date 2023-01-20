493216285aa4a4286f4fc0ab5ab2fb68fb94bc79
MarcosGG7 <doq97618@educastur.es>
2023-01-20 04:59:07 +0100

    Contenido index añadido. CSS index empezado

--- index.php
+++ index.php
@@ -3,11 +3,12 @@
     <head>
         <meta charset="UTF-8">
         <title>Bloom - JS</title>
+        <link rel="stylesheet" href="css/general.css">
     </head>
     <body>
         <header>
             <h1>BloomJS</h1>
-            <h2>"For a couple of Nice and Smooth tiny animations"</h2>
+            <h2><quote>Para que puedas olvidarte del teclado</quote></h2>
         </header>
         <nav>
             <ul>
@@ -21,22 +22,59 @@
         </nav>
         <main>
             <div id="contenido">
-                <section>
-
+                <section id="bienvenida">
+                    <h2>¡Bienvenido a BloomJS!</h2>
+                    <p>Bienvenido a BloomJS. Se trata de mi primera aplicación web de cara al público general. Si quieres saber más
+                    sobre mí puedes visitar el siguiente <a href="">enlace</a>, pero por ahora centrémonos en lo importante: ¿Qué
+                    es BloomJS?</p>
+                    <p>BloomJS, consiste en una aplicación web que te ayudará con tus proyectos de desarrollo web, permitiéndote aprovechar
+                    librerías gráficas potentes orientadas a entorno web, como WebGL para la generación de gráficos bonitos que podrás incrustar
+                    en tu web, de forma gratuita.</p> 
                 </section>
-                <section>
-
+                <section id="funcionalidades">
+                    <h2>¿Para qué sirve BloomJS?</h2>
+                    <p>Entre las funcionalidades más guays de BloomJS están:
+                    </p>
+                    <p>La generación con patrones 2D: se trata de generar patrones con la forma que tú mismo/a elijas de forma 
+                        aleatoria y mezclando diferentes imágenes para lograr un fondo único.
+                    </p>
+                    <p>La generación de escenas en 3D para colocarlas en tu página: como escuchas. Te permite subir un modelo3D,
+                        crear una escena con los modelos 3D que subas y exportar el resultado en un .js que podrás importar a tu
+                        proyecto.
+                    </p>
+                    <p>Generación de animaciones píxel: ¿Te gustaría lograr que en tu página llueva, los rayos del sol impidan leer
+                        bien el texto, las infinitas hojas no paren de caer en Otoño o tengas que rascar la nieve que se va acumulando
+                        en la pantalla durante el frío invierno? ¡BloomJS es tu solución!
+                    </p>
+                    <p>Compartir es vivir: comparte tus chulísimos gráficos generados bajo tus directrices con el resto de usuarios 
+                        y compite por llegar al top semanal de los mejores gráficos y diseños. Forma parte de la comunidad y disfruta
+                        valorando los diseños de otras personas y compartiendo información con ellos/as. Además, los mejores diseños
+                        semanales ¡Se recomendarán como plantillas a los nuevos usuarios de BloomJS!
+                    </p>
+                    <p>Generación de gráficos por shader: ¡Tu navegador también utiliza la GPU, aprovéchalo! Utiliza el generador de 
+                        gráficos por shader para lograr efectos que nunca podrías haber logrado sin librerías de gráficos avanzadas como
+                        WebGL: imagina tu página sobre automóviles de alta gama bajo el sol, con un sol cegador, la gasolina evaporándose
+                        y distorsionando los rayos de luz a su alrededor...
+                    </p>
                 </section>
-                <section>
-
+                <section id="empezar">
+                    <h2>¡Empecemos!</h2>
+                    <p>Utilizar BloomJS no es tan difícil como parece. Si te gusta ir a tu bola siéntete libre de explorar el resto
+                        del sitio libremente. Sin embargo, si te gustaría tener una experiencia más ordenada, te recomendamos que leas
+                        los artículos en la sección <a href="">Guías</a>. Espero que la aplicación te sea de utilidad. Por supuesto,
+                        si necesitas informar sobre un bug, atención de soporte o cualquier cosa no dudes en enviarnos un correo a
+                        <email>Correo</email> ¡Bienvenido/a a BloomJS!
+                    </p>
                 </section>
             </div>
             <div id="comentarios">
-            
+
             </div>
-            <footer>
-                
-            </footer>
         </main>
+        <footer>
+                Autor: <address>Marcos García García</address>
+                Correo: <mail>Correo@correo.com</mail>
+                <p>Copyright: Bloom&co&copy;</p>
+        </footer>
     </body>
 </html>
