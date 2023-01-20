c8b9c878b3867eece1b385560de50a8765e252a7
MarcosGG7 <doq97618@educastur.es>
2023-01-20 03:48:38 +0100

    Commit inicial

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Bloom - JS</title>
+    </head>
+    <body>
+        <header>
+            <h1>BloomJS</h1>
+            <h2>"For a couple of Nice and Smooth tiny animations"</h2>
+        </header>
+        <nav>
+            <ul>
+                <li><a href="">Inicio</a></li>
+                <li><a href="">Editor</a></li>
+                <li><a href="">Generador</a></li>
+                <li><a href="">Social</a></li>
+                <li><a href="">Guía de uso</a></li>
+                <li><a href="">Exportar</a></li>
+            </ul>
+        </nav>
+        <main>
+            <div id="contenido">
+                <section>
+
+                </section>
+                <section>
+
+                </section>
+                <section>
+
+                </section>
+            </div>
+            <div id="comentarios">
+            
+            </div>
+            <footer>
+                
+            </footer>
+        </main>
+    </body>
+</html>
